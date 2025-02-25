b4b4e445e4e4998bed65daca0c3e0ef16c76cc1c
hardy <luohf@infinilabs.com>
2025-02-25 21:17:28 +0800

    fix: for cat

--- scripts/env-init.sh
+++ scripts/env-init.sh
@@ -13,27 +13,33 @@ if [[ ! -z "$SSH_PRIVATE_KEY" ]]; then
 fi
 
 # for proxy
-[[ -z "$LOCAL_PORT" ]] || cat <<EOF > "$GITHUB_WORKSPACE/.oss.json"
-{
-    "local_port": $LOCAL_PORT,
-    "local_address": "127.0.0.1",
-    "servers": [
-        {
-            "server": "$CONNECT_SERVER",
-            "server_port": $CONNECT_PORT,
-            "password": "$CONNECT_KEY",
-            "timeout": $CONNECT_TIMEOUT,
-            "mode": "$CONNECT_MODE",
-            "method": "$CONNECT_METHOD"
-        }
-    ]
-}
-EOF && echo "Connection config setting done."
+if [[ ! -z "$LOCAL_PORT" ]]; then
+  cat <<-EOF > "$GITHUB_WORKSPACE/.oss.json"
+	{
+	  "local_port": $LOCAL_PORT,
+	  "local_address": "127.0.0.1",
+	  "servers": [
+	    {
+	      "server": "$CONNECT_SERVER",
+	      "server_port": $CONNECT_PORT,
+	      "password": "$CONNECT_KEY",
+	      "timeout": $CONNECT_TIMEOUT,
+	      "mode": "$CONNECT_MODE",
+	      "method": "$CONNECT_METHOD"
+	      }
+	  ]
+	}
+	EOF
+  echo "Connection config setting done."
+fi
 
 # for oss
-[[ -z "$OSS_EP" ]] || cat <<EOF > "$GITHUB_WORKSPACE/.oss.yml"
-endpoint: $OSS_EP
-accesskeyid: $OSS_AK
-accesskeysecret: $OSS_SK
-bucket: $OSS_BK
-EOF && echo "OSS config setting done."
+if [[ ! -z "$OSS_ENDPOINT" ]]; then
+  cat <<-EOF > "$GITHUB_WORKSPACE/.oss.yml"
+	endpoint: $OSS_EP
+	accesskeyid: $OSS_AK
+	accesskeysecret: $OSS_SK
+	bucket: $OSS_BK
+	EOF
+  echo "OSS config setting done."
+fi
