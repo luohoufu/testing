6e7a15dc51bec4b870ff7f6b32d27429b3859f34
hardy <luohf@infinilabs.com>
2025-02-27 21:42:19 +0800

    chore: for publish

--- scripts/go-matrix.sh
+++ scripts/go-matrix.sh
@@ -14,7 +14,7 @@ fi
 if [[ "${LOADGEN_PUBLISH}" == "true" ]]; then
   matrix_includes+=('{"product": "loadgen"}')
 fi
-if [[ "${FRAMEWORK_PUBLISH}" == "true" ]]; then
+if [[ "${FRAMEWORk_PUBLISH}" == "true" ]]; then
   matrix_includes+=('{"product": "framework"}')
 fi
 
