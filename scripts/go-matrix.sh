5307ea6931226a443e0739fd7ac8b17d939f9841
hardy <luohf@infinilabs.com>
2025-02-27 18:09:14 +0800

    chore: fix script pr

--- scripts/go-matrix.sh
+++ scripts/go-matrix.sh
@@ -2,23 +2,22 @@
 
 matrix_includes=()
 
-if [[ -n "$AGENT_PUBLISH" ]]; then
-  matrix_includes+=('{"product": "agent"}')
-fi
-if [[ -n "$CONSOLE_PUBLISH" ]]; then
-  matrix_includes+=('{"product": "console"}')
-fi
-if [[ -n "$GATEWAY_PUBLISH" ]]; then
-  matrix_includes+=('{"product": "gateway"}')
-fi
-if [[ -n "$LOADGEN_PUBLISH" ]]; then
-  matrix_includes+=('{"product": "loadgen"}')
-fi
-
 # if not triggered by workflow_dispatch, include all products
-if [[ "$GITHUB_EVENT_NAME" != "workflow_dispatch" ]]; then
-   matrix_includes=()
-   matrix_includes+=('{"product": "agent"}')
+if [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]]; then
+  if [[ -n "$AGENT_PUBLISH" ]]; then
+    matrix_includes+=('{"product": "agent"}')
+  fi
+  if [[ -n "$CONSOLE_PUBLISH" ]]; then
+    matrix_includes+=('{"product": "console"}')
+  fi
+  if [[ -n "$GATEWAY_PUBLISH" ]]; then
+    matrix_includes+=('{"product": "gateway"}')
+  fi
+  if [[ -n "$LOADGEN_PUBLISH" ]]; then
+    matrix_includes+=('{"product": "loadgen"}')
+  fi
+else
+  matrix_includes+=('{"product": "agent"}')
    matrix_includes+=('{"product": "console"}')
    matrix_includes+=('{"product": "gateway"}')
    matrix_includes+=('{"product": "loadgen"}')
